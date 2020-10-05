b183c8da33af5ba754a6349cd59cf21fd0b9e797
Raldylopez20 <raldysteven"hotmail.com>
2020-10-05 18:36:41 -0400

    Actualizaciont

---
+++ Entidades/Ciudades.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentMOdel.DataAnnotations;
+
+namespace Raldy_P1_APL.Entidades
+{
+
+    public class Ciudades
+    {
+
+    [Key]
+
+    public int CiudadId { get; set; }
+
+    }
+
+}
---
+++ Entidades/Nombre.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentMOdel.DataAnnotations;
+
+namespace Raldy_P1_APL.Entidades
+    {
+        [Key]
+
+        public string Nombre{ get; set; }
+
+    
+    }
